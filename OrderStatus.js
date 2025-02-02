29d7bfc4b9d00631429dc985d9c219a726ad6f96
Allan Gabriel <100225161+AllanGabri@users.noreply.github.com>
2025-02-01 22:31:56 -0500

    OrderStatus.js

---
+++ OrderStatus.js
@@ -0,0 +1,8 @@
+// src/context/orderStatus.js
+export const OrderStatus = {
+    PENDING: 'Pendente',
+    IN_PROGRESS: 'Em Preparação',
+    OUT_FOR_DELIVERY: 'Em Entrega',
+    COMPLETED: 'Concluído',
+  };
+  
